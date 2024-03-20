d71ecc21f4a498be97a3dd645fb2183342612ad5
harry-breslin <hbre0004@student.monash.edu>
2024-03-20 19:12:06 +1100

    Add SMTP setup and sendEmail function

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ function toggleMenu() {
 }
 
 // TODO: Create sendEmail()
+function sendEmail() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
 
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "hbre0004@student.monash.edu",
+        Password: "password",
+        To: "hbre0004@student.monash.edu",
+        From: email,
+        Subject: `New Website Enquiry from ${name}
+        `,
+        Body: message
+    }).then(function (message) {
+        // Reset form after successful submission
+        alert("Email sent successfully!");
+    });
+}
 
 // TODO: Create reset()
